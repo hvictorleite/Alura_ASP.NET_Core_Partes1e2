32ab944129c0b5dcc09361b1670e5640987aa8d4
hvictor186 <hvictorleite@gmail.com>
2022-08-30 15:05:35 -0300

    Obtidos livros de 'livros.json' e armazenados no BD.

--- Aulas/Aula1/CasaDoCodigo/DataService.cs
+++ Aulas/Aula1/CasaDoCodigo/DataService.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CasaDoCodigo
 {
@@ -14,6 +18,21 @@ namespace CasaDoCodigo
         public void InicializaBD()
         {
             contexto.Database.Migrate();
+            var json = File.ReadAllText("livros.json");
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+
+            foreach (var livro in livros)
+            {
+                contexto.Set<Produto>().Add(new Produto(livro.Codigo, livro.Nome, livro.Preco));
+            }
+            contexto.SaveChanges();
         }
+    }
+
+    class Livro
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
     }
 }
