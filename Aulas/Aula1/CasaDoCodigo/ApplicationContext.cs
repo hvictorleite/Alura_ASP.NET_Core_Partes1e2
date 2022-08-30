5592575d0f0c3d59e2d4a696fd467b7eee240202
hvictor186 <hvictorleite@gmail.com>
2022-08-30 14:01:00 -0300

    Mapeando relacionamento das tabelas no BD.

--- Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
+++ Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
@@ -17,8 +17,31 @@ namespace CasaDoCodigo
         {
             base.OnModelCreating(modelBuilder);
 
+            // Produto
             modelBuilder.Entity<Produto>()
                 .HasKey(p => p.Id);
+
+            // Pedido
+            modelBuilder.Entity<Pedido>()
+                .HasKey(p => p.Id);
+            modelBuilder.Entity<Pedido>()
+                .HasMany(p => p.Itens).WithOne(p => p.Pedido);
+            modelBuilder.Entity<Pedido>()
+                .HasOne(p => p.Cadastro).WithOne(p => p.Pedido);
+
+            // ItemPedido
+            modelBuilder.Entity<ItemPedido>()
+                .HasKey(ip => ip.Id);
+            modelBuilder.Entity<ItemPedido>()
+                .HasOne(ip => ip.Pedido);
+            modelBuilder.Entity<ItemPedido>()
+                .HasOne(ip => ip.Produto);
+
+            // Cadastro
+            modelBuilder.Entity<Cadastro>()
+                .HasKey(c => c.Id);
+            modelBuilder.Entity<Cadastro>()
+                .HasOne(c => c.Pedido);
         }
     }
 }
